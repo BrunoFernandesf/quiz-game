c5051455d280b281f9908df4f5424c9aa9215862
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-07-26 13:24:53 -0300

    adicionando var para imagens dinâmicas

--- scripts/QuizResource.gd
+++ scripts/QuizResource.gd
@@ -1,5 +1,6 @@
 class_name QuizQuestion extends Resource
 
 @export var question_info: String
+@export var question_image: Texture2D
 @export var question_amount: Array[String]
 @export var question_correct: String
